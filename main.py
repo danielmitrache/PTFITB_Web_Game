4ca500265d93df3fc9ed0b6f8de7e6f4700e9f22
daniel21 <mitracheadaniel@gmail.com>
2024-12-26 16:32:15 +0000

    level validation + better splashscreens + more music + better difficulty
     - added a bfs algorithm to validate levels
     - added more bg tracks
     - made improvements to overall art and design
     - added more difficulty variation, game is more exciting
     - fixed mute/unmute bugs

--- main.py
+++ main.py
@@ -133,8 +133,14 @@ class GirlEnemy:
     def draw(self, win):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+    def get_velocity(self):
+        return self.vel
+
+    def set_velocity(self, vel):
+        self.vel = vel
+
     @classmethod
-    def spawn_random(cls, player, walls, water_obstacles, bag, fries):
+    def spawn_random(cls, player, walls, water_obstacles, bag, fries, girl_vel):
         max_distance = -1
         girl_enemy_pos = (-1, -1)
         for i in range(10):
@@ -158,7 +164,7 @@ class GirlEnemy:
 
         if girl_enemy_pos == (-1, -1):
             return None
-        girl_enemy = cls(*girl_enemy_pos)
+        girl_enemy = cls(*girl_enemy_pos, vel=girl_vel)
         return girl_enemy
 
     def move(self, player, walls):
@@ -285,6 +291,10 @@ class Bag:
     def draw(self, win):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+    def remove(self):
+        self.rect.x = -200
+        self.rect.y = -200
+
     @classmethod
     def generate_random_bag(cls, player, walls, water_obstacles):
         width, height = 50, 50
@@ -297,9 +307,23 @@ class Bag:
                 return bag
 
 py.mixer.init()
-py.mixer.music.load('Sounds/bg-music.ogg')
-py.mixer.music.play(-1)
 py.mixer.music.set_volume(0.1)
+
+bg_music_tracks = [
+    {
+        'path': 'Sounds/bg-music.ogg',
+        'duration': 64
+    },
+    {
+        'path': 'Sounds/bg-music-2.ogg',
+        'duration': 64
+    },
+    {
+        'path': 'Sounds/bg-music-3.ogg',
+        'duration': 64
+    }
+]
+current_track_index = -1
 
 sound_effects = {
     'complete-level': py.mixer.Sound('Sounds/complete-level.ogg'),
@@ -317,24 +341,33 @@ WATER_SPAWN_MIN_DISTANCE = 100
 max_time_level = MAX_TIME_LEVEL
 deodorant_launch_event_speed = 3000
 deodorant_speed = 5
+girl_vel = 1
+music_volume_on = True
 
 #Events:
 DEODORANT_LAUNCH_EVENT = py.USEREVENT + 1
 RESET_DEODORANT_LAUNCH_TIME_EVENT = py.USEREVENT + 2
+MUSIC_END_EVENT = py.USEREVENT + 3
+FIX_LVL_COMPLETE_SFX = py.USEREVENT + 4
+MUTE_COMPLETE_LEVEL_SFX = py.USEREVENT + 5
 
 async def main():
     await main_menu(WIN)
+    global music_volume_on
+    play_next_track()
     run = True
     game_paused = False
     score = 0
     high_score = 0
+    num_water_obstacles = 3
+    num_walls = 10
     player_start_x = WIDTH // 2 - PLAYER_WIDTH // 2
     player_start_y = HEIGHT // 2 - PLAYER_HEIGHT // 2
     player = Player(player_start_x, player_start_y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_VEL)
     clock = py.time.Clock()
     start_time = time.time()
     timer_start = py.time.get_ticks()
-    walls, water_obstacles, bag, fries, enemy_girl = generate_level(10, 3, player, WATER_SPAWN_MIN_DISTANCE, score)
+    walls, water_obstacles, bag, fries, enemy_girl = generate_level(num_walls, num_water_obstacles, player, WATER_SPAWN_MIN_DISTANCE, score)
     player_can_pass = False
     launch_deodorant = False
     play_sound_effects = True
@@ -342,6 +375,10 @@ async def main():
     remaining_deodorant_time = deodorant_launch_event_speed
     deodorant = Deodorant(-1, -1, 'up')
     bg_saturation = 0.2
+
+    #Music setup
+    py.time.set_timer(MUSIC_END_EVENT, bg_music_tracks[current_track_index]['duration'] * 1000)
+
     while run:
         clock.tick(60)
         await asyncio.sleep(0)
@@ -359,8 +396,8 @@ async def main():
                 break
             else:
                 score, player_can_pass, launch_deodorant, timer_start, bg_saturation = reset_game(player, score, player_can_pass, launch_deodorant, timer_start, bg_saturation, deodorant)
-                reset_difficulty()
-                walls, water_obstacles, bag, fries, enemy_girl = generate_level(10, 3, player, WATER_SPAWN_MIN_DISTANCE, score)
+                num_walls, num_water_obstacles = reset_difficulty()
+                walls, water_obstacles, bag, fries, enemy_girl = generate_level(num_walls, num_water_obstacles, player, WATER_SPAWN_MIN_DISTANCE, score)
 
         for event in py.event.get():
             if event.type == py.QUIT:
@@ -386,9 +423,19 @@ async def main():
                 py.time.set_timer(RESET_DEODORANT_LAUNCH_TIME_EVENT, 999999999)
             elif event.type == py.KEYDOWN and event.key == py.K_m:
                 py.mixer.music.set_volume(0.0 if py.mixer.music.get_volume() > 0.0 else 0.1)
+                music_volume_on = not music_volume_on
             elif event.type == py.KEYDOWN and event.key == py.K_n:
                 for sound in sound_effects.values():
                     sound.set_volume(0.0 if sound.get_volume() > 0.0 else 0.1)
+            elif event.type == MUSIC_END_EVENT:
+                play_next_track()
+                py.time.set_timer(MUSIC_END_EVENT, bg_music_tracks[current_track_index]['duration'] * 1000)
+            elif event.type == MUTE_COMPLETE_LEVEL_SFX:
+                sound_effects['complete-level'].set_volume(0.0)
+                py.time.set_timer(MUTE_COMPLETE_LEVEL_SFX, 999999999)
+            elif event.type == FIX_LVL_COMPLETE_SFX:
+                sound_effects['complete-level'].set_volume(0.1)
+                py.time.set_timer(FIX_LVL_COMPLETE_SFX, 999999999)
 
         if game_paused:
             draw_pause_screen(WIN, bg_color)
@@ -405,7 +452,10 @@ async def main():
             player_can_pass = True
 
         if player.check_bag_colision(bag) and player_can_pass:
+            bag.remove()
             sound_effects['complete-level'].play()
+            py.time.set_timer(MUTE_COMPLETE_LEVEL_SFX, 1)
+            py.time.set_timer(FIX_LVL_COMPLETE_SFX, 500)
             score += 1
             if score == 1:
                 py.time.set_timer(DEODORANT_LAUNCH_EVENT, deodorant_launch_event_speed)
@@ -413,10 +463,15 @@ async def main():
             player.set_image(py.transform.scale(py.image.load('Images/character_front_sprite.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)))
             if score % 5 == 0:
                 update_difficulty(DEODORANT_LAUNCH_EVENT)
+            if score % 10 == 0:
+                num_walls, num_water_obstacles = increase_difficulty_10(num_walls, num_water_obstacles)
+            if score % 20 == 0:
+                global girl_vel
+                girl_vel = min(2, girl_vel + 1)
             high_score = max(score, high_score)
             timer_start = py.time.get_ticks()
             player_can_pass = False
-            walls, water_obstacles, bag, fries, enemy_girl = generate_level(10, 3, player, WATER_SPAWN_MIN_DISTANCE, score)
+            walls, water_obstacles, bag, fries, enemy_girl = generate_level(num_walls, num_water_obstacles, player, WATER_SPAWN_MIN_DISTANCE, score)
 
         if player.check_obstacle_colisions(water_obstacles) or player.rect.colliderect(enemy_girl.rect):
             sound_effects['hit'].play()
@@ -427,8 +482,8 @@ async def main():
                 break
             else:
                 score, player_can_pass, launch_deodorant, timer_start, bg_saturation = reset_game(player, score, player_can_pass, launch_deodorant, timer_start, bg_saturation, deodorant)
-                reset_difficulty()
-                walls, water_obstacles, bag, fries, enemy_girl = generate_level(10, 3, player, WATER_SPAWN_MIN_DISTANCE, score)
+                num_walls, num_water_obstacles = reset_difficulty()
+                walls, water_obstacles, bag, fries, enemy_girl = generate_level(num_walls, num_water_obstacles, player, WATER_SPAWN_MIN_DISTANCE, score)
 
         if launch_deodorant and deodorant.rect.colliderect(player.rect):
             sound_effects['hit'].play()
@@ -439,7 +494,7 @@ async def main():
                 break
             else:
                 score, player_can_pass, launch_deodorant, timer_start, bg_saturation = reset_game(player, score, player_can_pass, launch_deodorant, timer_start, bg_saturation, deodorant)
-                reset_difficulty()
+                num_walls, num_water_obstacles = reset_difficulty()
                 walls, water_obstacles, bag, fries, enemy_girl = generate_level(10, 3, player, WATER_SPAWN_MIN_DISTANCE, score)
         if not game_paused:
             draw_sprites(player, player_can_pass, bg_color, walls, water_obstacles, bag, fries, launch_deodorant, deodorant)
@@ -454,14 +509,19 @@ def get_background_underlay(t, saturation=0.2):
     return int(r * 255), int(g * 255), int(b * 255)
 
 def generate_level(num_walls, num_obstacles, player, min_distance, score):
-    walls = Wall.generate_random_walls(num_walls, 50, 100, player)
-    water_obstacles = WaterObstacle.generate_random_obstacles(num_obstacles, player, walls, min_distance)
-    bag = Bag.generate_random_bag(player, walls, water_obstacles)
-    fries = Fries.generate_random_fries_location(player, walls, water_obstacles, bag)
+    while True:
+        #await asyncio.sleep(0)
+        walls = Wall.generate_random_walls(num_walls, 50, 100, player)
+        water_obstacles = WaterObstacle.generate_random_obstacles(num_obstacles, player, walls, min_distance)
+        bag = Bag.generate_random_bag(player, walls, water_obstacles)
+        fries = Fries.generate_random_fries_location(player, walls, water_obstacles, bag)
+        if bfs(player.rect, fries.rect, walls, water_obstacles) and bfs(player.rect, bag.rect, walls, water_obstacles):
+            break
     if score >= 5:
-        enemy_girl = GirlEnemy.spawn_random(player, walls, water_obstacles, bag, fries)
+        enemy_girl = GirlEnemy.spawn_random(player, walls, water_obstacles, bag, fries, girl_vel)
     else:
         enemy_girl = GirlEnemy(-100, -100, vel=0)
+
     return walls, water_obstacles, bag, fries, enemy_girl
 
 def reset_game(player, score, player_can_pass, launch_deodorant, timer_start, bg_saturation, deodorant):
@@ -530,17 +590,20 @@ def draw_enemy_girl(enemy_girl, player, walls):
 
 def update_difficulty(DEODORANT_LAUNCH_EVENT):
     global max_time_level, deodorant_launch_event_speed, deodorant_speed
-    max_time_level = max(7500, max_time_level - 500)
+    max_time_level = max(7500, max_time_level - 1000)
     deodorant_speed = min(8, deodorant_speed + 1)
     py.time.set_timer(DEODORANT_LAUNCH_EVENT, max(deodorant_launch_event_speed - 500, 1000))
 
-def reset_difficulty():
+def reset_difficulty(num_walls=10, num_water_obstacles=3):
     global WATER_SPAWN_MIN_DISTANCE
     WATER_SPAWN_MIN_DISTANCE = 100
-    global max_time_level, deodorant_launch_event_speed, deodorant_speed
+    global max_time_level, deodorant_launch_event_speed, deodorant_speed, girl_vel
+    girl_vel = 1
     max_time_level = MAX_TIME_LEVEL
     deodorant_launch_event_speed = 3000
     deodorant_speed = 5
+    num_walls, num_water_obstacles = 10, 3
+    return num_walls, num_water_obstacles
 
 def draw_pause_screen(win, bg_color):
     win.fill(bg_color)
@@ -555,6 +618,8 @@ def draw_pause_screen(win, bg_color):
 
 async def main_menu(win):
     run = True
+    global music_volume_on
+    play_next_track()
     main_menu_bg = py.transform.scale2x(py.image.load('Images/main_menu_bg.png'))
     while run:
         await asyncio.sleep(0)
@@ -570,18 +635,21 @@ async def main_menu(win):
         text = font.render('in the Bag!', 1, (255, 0, 0))
         win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2 - 150))
 
-        wasd_keys_image = py.transform.scale(py.image.load('Images/wasd.png'), (100, 100))
-        win.blit(wasd_keys_image, (WIDTH//2 - wasd_keys_image.get_width()//2 - 100, HEIGHT//2 - wasd_keys_image.get_height()//2))
-
-        esc_key_image = py.transform.scale(py.image.load('Images/esc.png'), (50, 50))
-        win.blit(esc_key_image, (WIDTH//2 + esc_key_image.get_width()//2 + 100, HEIGHT//2 - esc_key_image.get_height()//2))
+        white_rect = py.Rect(0, HEIGHT // 2 -50, WIDTH, 200)
+        py.draw.rect(win, (255, 255, 255), white_rect)
 
         font = py.font.Font(FONT_FAMILY, 30)
-        text = font.render('TO MOVE', 1, (0, 0, 0))
-        win.blit(text, (WIDTH//2 - wasd_keys_image.get_width()//2 - 100 - (text.get_width() - wasd_keys_image.get_width())//2, HEIGHT//2 - text.get_height()//2 + 50))
+        text = font.render('MOVE : WASD / Arrow Keys', 1, (0, 0, 0))
+        win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2 - 5))
 
-        text = font.render('TO PAUSE', 1, (0, 0, 0))
-        win.blit(text, (WIDTH//2 + esc_key_image.get_width()//2 + 100 - (text.get_width() - esc_key_image.get_width())//2, HEIGHT//2 - text.get_height()//2 + 50))
+        text = font.render('PAUSE : ESC', 1, (0, 0, 0))
+        win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2 + 30))
+
+        text = font.render('MUTE / UNMUTE MUSIC : M', 1, (0, 0, 0))
+        win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2 + 70))
+
+        text = font.render('MUTE / UNMUTE SOUND EFFECTS : N', 1, (0, 0, 0))
+        win.blit(text, (WIDTH//2 - text.get_width()//2, HEIGHT//2 - text.get_height()//2 + 110))
 
         py.display.update()
         for event in py.event.get():
@@ -589,6 +657,9 @@ async def main_menu(win):
                 run = False
             if event.type == py.KEYDOWN:
                 run = False
+            elif event.type == MUSIC_END_EVENT:
+                play_next_track()
+                py.time.set_timer(MUSIC_END_EVENT, bg_music_tracks[current_track_index]['duration'] * 1000)
 
 async def game_over_screen(win, score, high_score):
     win.fill((0, 0, 0))  # Fill the screen with black
@@ -617,5 +688,43 @@ async def game_over_screen(win, score, high_score):
             if event.type == py.KEYDOWN:
                 if event.key == py.K_RETURN:
                     return "restart"
+            elif event.type == MUSIC_END_EVENT:
+                play_next_track()
+                py.time.set_timer(MUSIC_END_EVENT, bg_music_tracks[current_track_index]['duration'] * 1000)
+
+def play_next_track():
+    global current_track_index, music_volume_on
+    current_track_index = (current_track_index + 1) % len(bg_music_tracks)
+    py.mixer.music.load(bg_music_tracks[current_track_index]['path'])
+    py.mixer.music.set_volume(0.1) if music_volume_on else py.mixer.music.set_volume(0.0)
+    py.mixer.music.play(-1)
+
+def bfs(start, goal, walls, obstacles):
+    queue = [start]
+    visited = set()
+    visited.add((start.x, start.y))
+
+    while queue:
+        current = queue.pop(0)
+        if current.colliderect(goal):
+            return True
+
+        neighbors = [
+            py.Rect(current.x + PLAYER_VEL, current.y, current.width, current.height),
+            py.Rect(current.x - PLAYER_VEL, current.y, current.width, current.height),
+            py.Rect(current.x, current.y + PLAYER_VEL, current.width, current.height),
+            py.Rect(current.x, current.y - PLAYER_VEL, current.width, current.height)
+        ]
+
+        for neighbor in neighbors:
+            if (neighbor.x, neighbor.y) not in visited and not neighbor.collidelistall([wall.rect for wall in walls]) and not neighbor.collidelistall([obstacle.rect for obstacle in obstacles]):
+                queue.append(neighbor)
+                visited.add((neighbor.x, neighbor.y))
+
+    return False
+
+def increase_difficulty_10(num_walls, num_water_obstacles):
+    num_water_obstacles += 1
+    return num_walls, num_water_obstacles
 
 asyncio.run(main())
